985fa1da08017377a3ee898a61bfb20fccb3d692
Thomas Foetisch <thomas.foetisch@epfl.ch>
2017-03-24 10:25:16 +0100

    Complete the standard directory structure

---
+++ config.mk
@@ -0,0 +1,25 @@
+CXX = g++
+DEPS_BIN = g++
+CXXFLAGS = -g -std=c++11
+LDFLAGS = -g
+AR = ar
+ARFLAGS = rc
+
+PREFIX = ~/.local/
+BIN_DIR = bin/
+INCLUDE_DIR = include/
+LIB_DIR = lib/
+
+
+SOURCES = 
+
+HEADERS = 
+
+BIN = 
+
+
+#bin/...: ...
+
+LIB = 
+
+#lib/...: ...
